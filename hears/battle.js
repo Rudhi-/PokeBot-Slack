7ea1bf5beed5ba3fdca689204966fc29e11c38d4
Rudhi- <anirud3@vt.edu>
2016-10-03 21:40:29 -0400

    Added a lot of stuff
    lots of stuff added

--- hears/battle.js
+++ hears/battle.js
@@ -2,26 +2,239 @@
  * Created by Anirudha Simha.
  * Still under development
  */
-module.exports={
-    battle : function(bot, message){
-        bot.startConversation(message,function(err,convo){
-            convo.ask("who would you like to battle?",function(response,convo){
-                convo.say("Asking "+response.text+" if they would like to battle!");
-                bot.api.users.list({
-                    "presence":"1"
-                }, function(err, res) {
-                    if (err) {
-                       convo.say('Failed to find user :(');
+// 0only works against pokebot
+module.exports = {
+    startBattle: function (bot, message) {
+        //var challangeThem = false;
+        //var challanged = "";
+        //var challanger = "";
+        var playerTeam = {};
+        var cpuTeam = {};
+
+        bot.startConversation(message, function (err, convo) {
+            convo.ask({
+                    attachments: [
+                    {
+                        title: 'Are you sure you wish to battle?',
+                        callback_id: '123',
+                        attachment_type: 'default',
+                        actions: [
+                            {
+                                "name": "yes",
+                                "text": "Yes",
+                                "value": "yes",
+                                "type": "button"
+                            },
+                            {
+                                "name": "no",
+                                "text": "No",
+                                "value": "no",
+                                "type": "button"
+                            }
+                        ]
                     }
-                    else{
-                        //onvo.say(JSON.stringify(res,null,4));
-                        for(var i = 0; i < res.members.length;i++){
-                            //if(res.members[i])
+                ]
+                },[
+                    {
+                        pattern: "yes",
+                        callback: function (response, convo) {
+                            //convo.say("Solid! Let us battle!");
+                            //console.log("GETTING POKEMON!");
+                            bot.reply(message, "Ok hold on getting your pokemon! (This may take a minute or so)");
+                            getPokemon(function (result) {
+                                playerTeam = result;
+                                //console.log(playerTeam);
+                                bot.reply(message, "Here are your pokemon!!");
+                                bot.reply(message, JSON.stringify(playerTeam, null, 4));
+                                bot.reply(message, "Generating opponent's team!! (This may take a minute or so)");
+                                getPokemon(function (result) {
+                                    bot.reply(message, "_Solid Let's fight!!_");
+                                    cpuTeam = result;
+                                    battleStart(bot, message, playerTeam, cpuTeam, function () {
+                                        convo.next();
+                                    });
+
+                                });
+
+                            });
+                        }
+                    },
+                    {
+                        pattern: "no",
+                        callback: function (response, convo) {
+                            convo.say("Alright no problem then!");
+                            convo.next();
+                        }
+                    },
+                    {
+                        default: true,
+                        callback: function (response, convo) {
+                            // just repeat the question
+                            convo.repeat();
+                            convo.next();
                         }
                     }
-                });
-                convo.next();
-            });
+                ]);
+            //convo.next();
         });
+
+    }
+
+};
+
+/*
+ function teamSelector(cb){
+ var async = require('../node_modules/async');
+ var team = {};
+ team.pokemon = [];
+ var request = require("../node_modules/request");
+ async.parallel([
+ getPokemon(team,0,function(){
+ console.log(JSON.stringify(team.pokemon[0],null,4));
+ }),
+ getPokemon(team,1,function(){
+ console.log(JSON.stringify(team.pokemon[1],null,4));
+ }),
+ getPokemon(team,2,function(){
+ console.log(JSON.stringify(team.pokemon[2],null,4));
+ }),
+ getPokemon(team,3,function(){
+ console.log(JSON.stringify(team.pokemon[3],null,4));
+ }),
+ getPokemon(team,4,function(){
+ console.log(JSON.stringify(team.pokemon[4],null,4));
+ }),
+ getPokemon(team,5,function(){
+ console.log(JSON.stringify(team.pokemon[5],null,4));
+ })
+ ],function(err){
+ console.log(JSON.stringify(team,null,4));
+ })
+ }
+ */
+
+function battleStart(bot, message, playerTeam, cpuTeam, callback) {
+    bot.reply(message, "In func _battleStart_!");
+    bot.reply(message, "Your move");
+
+
+    if (callback) {
+        callback();
     }
 }
+function getPokemon(callback) {
+    var team = {};
+    team.pokemon = [];
+    team.items = [];
+    var request = require('../node_modules/request');
+
+    // Getting pokemon 1
+    var id = pad(Math.floor(Math.random() * 650), 3);
+    request("http://pokeapi.co/api/v2/pokemon/" + id, function (error, response, body) {
+        console.log(JSON.parse(body).name);
+        team.pokemon[0] = {};
+        team.pokemon[0].name = JSON.parse(body).name;
+        // getting all of the stats for the pokemon
+        for (var j = 0; j < JSON.parse(body).stats.length; j++) {
+            team.pokemon[0][JSON.parse(body).stats[j].stat.name] = JSON.parse(body).stats[j].base_stat;
+        }
+        // Getting pokemon 2
+        id = pad(Math.floor(Math.random() * 650), 3);
+
+        request("http://pokeapi.co/api/v2/pokemon/" + id, function (error, response, body) {
+            console.log(JSON.parse(body).name);
+            team.pokemon[1] = {};
+            team.pokemon[1].name = JSON.parse(body).name;
+            // getting all of the stats for the pokemon
+            for (var j = 0; j < JSON.parse(body).stats.length; j++) {
+                team.pokemon[1][JSON.parse(body).stats[j].stat.name] = JSON.parse(body).stats[j].base_stat;
+            }
+
+            // Getting pokemon 3
+            id = pad(Math.floor(Math.random() * 650), 3);
+
+            request("http://pokeapi.co/api/v2/pokemon/" + id, function (error, response, body) {
+                console.log(JSON.parse(body).name);
+                team.pokemon[2] = {};
+                team.pokemon[2].name = JSON.parse(body).name;
+                // getting all of the stats for the pokemon
+                for (var j = 0; j < JSON.parse(body).stats.length; j++) {
+                    team.pokemon[2][JSON.parse(body).stats[j].stat.name] = JSON.parse(body).stats[j].base_stat;
+                }
+
+                // pokemon 4
+                id = pad(Math.floor(Math.random() * 650), 3);
+                request("http://pokeapi.co/api/v2/pokemon/" + id, function (error, response, body) {
+                    console.log(JSON.parse(body).name);
+                    team.pokemon[3] = {};
+                    team.pokemon[3].name = JSON.parse(body).name;
+                    // getting all of the stats for the pokemon
+                    for (var j = 0; j < JSON.parse(body).stats.length; j++) {
+                        team.pokemon[3][JSON.parse(body).stats[j].stat.name] = JSON.parse(body).stats[j].base_stat;
+                    }
+
+                    // pokemon 5
+                    id = pad(Math.floor(Math.random() * 650), 3);
+                    request("http://pokeapi.co/api/v2/pokemon/" + id, function (error, response, body) {
+                        console.log(JSON.parse(body).name);
+                        team.pokemon[4] = {};
+                        team.pokemon[4].name = JSON.parse(body).name;
+                        // getting all of the stats for the pokemon
+                        for (var j = 0; j < JSON.parse(body).stats.length; j++) {
+                            team.pokemon[4][JSON.parse(body).stats[j].stat.name] = JSON.parse(body).stats[j].base_stat;
+                        }
+
+                        // pokemon 6
+                        id = pad(Math.floor(Math.random() * 650), 3);
+                        request("http://pokeapi.co/api/v2/pokemon/" + id, function (error, response, body) {
+                            console.log(JSON.parse(body).name);
+                            team.pokemon[5] = {};
+                            team.pokemon[5].name = JSON.parse(body).name;
+                            // getting all of the stats for the pokemon
+                            for (var j = 0; j < JSON.parse(body).stats.length; j++) {
+                                team.pokemon[5][JSON.parse(body).stats[j].stat.name] = JSON.parse(body).stats[j].base_stat;
+                            }
+                            //var err = null;
+                            var result = team;
+                            //console.log("DONE");
+                            //console.log(JSON.stringify(team,null,4));
+                            if (callback) {
+                                return callback(result);
+                            }
+                        });
+                    });
+                });
+            });
+        });
+
+
+    });
+
+}
+
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+/*
+ function(response,convo){
+ challanger = message.user;
+ challanged = (response.text+"").trim().toLowerCase();
+ convo.say("Asking "+challanged+" if they would like to battle!");
+ bot.api.users.list({
+ "presence":"1"
+ }, function(err, res) {
+ if (err) {
+ convo.say('Failed to find user :(');
+ }
+ else{
+ bot.say({
+ "text":"You have been challanged, to accept this type battle accepted in a direct message with your challanger "+challanger,
+ "channel":"@"+challanged
+ })
+ }
+ });
+ convo.next();
+
+ }*/
--- main.js
+++ main.js
@@ -2,40 +2,73 @@
  * Created by Anirudha Simha
  */
 
-var Botkit = require('./node_modules/botkit');
-var controller = Botkit.slackbot({
-    debug: false
+var controller = require('./node_modules/botkit').slackbot({
+    interactive_replies: true
 });
+/*
+// set up a botkit app to expose oauth and webhook endpoints
+controller.setupWebserver(8082, function (err, webserver) {
+
+    // set up web endpoints for oauth, receiving webhooks, etc.
+    controller
+    //.createHomepageEndpoint(controller.webserver)
+        .createOauthEndpoints(webserver, function (err, req, res) {
+
+        });
+    //.createWebhookEndpoints(controller.webserver);
+
+});*/
 var bot = controller.spawn({
     token: require("./properties.json").slack_token
 }).startRTM();
 
+var localtunnel = require('./node_modules/localtunnel');
+
+var tunnel = localtunnel(8082, {"subdomain":"pokebot"+require("./properties.json").localtunnel_subdomain}, function (err, tunnel) {
+    if (err) {
+        console.log("ERROR: " + err);
+    }
+    // the assigned public url for your tunnel
+    console.log(tunnel.url);
+});
+
+tunnel.on('close', function () {
+    // tunnels are closed
+    console.log("Tunnel closed!");
+});
+
 //the regex will only match pokemon names
-controller.hears([/I choose you ([^0-9]+)/i,/go ([^0-9]+)/i ], ['direct_message', 'direct_mention', 'mention','ambient'], function(bot, message) {
-
-    var request = require("request");
+controller.hears([/I choose you ([^0-9]+)/i, /go ([^0-9]+)/i], ['direct_message', 'direct_mention', 'mention', 'ambient'], function (bot, message) {
+    var request = require("./node_modules/request");
     var pokemon = message.match[1].toLowerCase().trim();
-    request("http://pokeapi.co/api/v2/pokemon/"+pokemon, function(error, response, body) {
-        console.log(JSON.stringify(JSON.parse(body),null,4))
-        bot.reply(message,{
-            "username":"pokebot",
-            "attachments": [
-                {
-                    "title": pokemon,
-                    "title_link": "http://sprites.pokecheck.org/i/"+pad(new Number(JSON.parse(body).id),3)+".gif",
-                    "image_url": "http://sprites.pokecheck.org/i/"+pad(new Number(JSON.parse(body).id),3)+".gif"
-                }
-            ]
-        })
+    request("http://pokeapi.co/api/v2/pokemon/" + pokemon, function (error, response, body) {
+        if (JSON.parse(body).detail != "Not found.") {
+            bot.reply(message, {
+                "username": "pokebot",
+                "attachments": [
+                    {
+                        "title": pokemon,
+                        "title_link": "http://sprites.pokecheck.org/i/" + pad(Number(JSON.parse(body).id), 3) + ".gif",
+                        "image_url": "http://sprites.pokecheck.org/i/" + pad(Number(JSON.parse(body).id), 3) + ".gif"
+                    }
+                ]
+            });
+        }
+        else {
+            bot.reply(message, "Pokemon not found! Did you type its name correctly?");
+        }
     });
-
 });
 
-controller.hears([/hello/i],['direct_message', 'direct_mention', 'mention','ambient'],function(bot,message){
-    //require('./hears/battle').battle(bot,message);
-    bot.reply(message,"Hello I'm pokebot!\n Type _I choose you_ or _go_ followed by a pokemon's name to summon a pokemon!");
+controller.hears([/hello/i], ['direct_message', 'direct_mention', 'mention', 'ambient'], function (bot, message) {
+    bot.reply(message, "Hello I'm pokebot!\n" +
+        "Type _I choose you_ or _go_ followed by a pokemon's name to summon a pokemon!\n" +
+        "Or if you wish to fight me than type _battle_ in a direct message to me!");
 });
 
+controller.hears([/battle/i], ['direct_message'], function (bot, message) {
+    require('./hears/battle.js').startBattle(bot, message);
+});
 
 function pad(n, width, z) {
     z = z || '0';
