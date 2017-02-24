29315f69cb41d42ec6da2c5d8355a786247cbb90
Rudhi- <anirud3@vt.edu>
2017-02-24 18:54:56 -0500

    Some updates
    Need to continue development on this again, pushing to repo so that I can access and develop from another system

--- main.js
+++ main.js
@@ -4,20 +4,26 @@
 
 var controller = require('./node_modules/botkit').slackbot({
     interactive_replies: true
+   //
 });
-/*
+
+controller.configureSlackApp({
+    clientId : require("./properties.json").client_id,
+    clientSecret : require("./properties.json").client_secret,
+    redirectUri: 'http://localhost:3002',
+    scopes: ['incoming-webhook','team:read','users:read','channels:read','im:read','im:write','groups:read','emoji:read','chat:write:bot']
+});
+
 // set up a botkit app to expose oauth and webhook endpoints
-controller.setupWebserver(8082, function (err, webserver) {
+controller.setupWebserver(8082,function(err,webserver) {
 
     // set up web endpoints for oauth, receiving webhooks, etc.
     controller
-    //.createHomepageEndpoint(controller.webserver)
-        .createOauthEndpoints(webserver, function (err, req, res) {
+        .createHomepageEndpoint(controller.webserver)
+        .createOauthEndpoints(controller.webserver,function(err,req,res) { })
+        .createWebhookEndpoints(controller.webserver);
 
-        });
-    //.createWebhookEndpoints(controller.webserver);
-
-});*/
+});
 var bot = controller.spawn({
     token: require("./properties.json").slack_token
 }).startRTM();
@@ -38,31 +44,10 @@ tunnel.on('close', function () {
 });
 
 //the regex will only match pokemon names
-controller.hears([/I choose you ([^0-9]+)/i, /go ([^0-9]+)/i], ['direct_message', 'direct_mention', 'mention', 'ambient'], function (bot, message) {
+controller.hears([/I choose you ([a-z]+)/i, /go ([a-z]+)/i], ['direct_message', 'direct_mention', 'mention', 'ambient'], function (bot, message) {
     var request = require("./node_modules/request");
     var pokemon = message.match[1].toLowerCase().trim();
-    //This site no longer works :(
-    /*request("http://pokeapi.co/api/v2/pokemon/" + pokemon, function (error, response, body) {
-        if (JSON.parse(body).detail != "Not found.") {
-            bot.reply(message, {
-                "username": "pokebot",
-                "attachments": [
-                    {
-                        "title": pokemon,
-                       "title_link": "http://sprites.pokecheck.org/i/" + pad(Number(JSON.parse(body).id), 3) + ".gif",
-                       "image_url": "http://sprites.pokecheck.org/i/" + pad(Number(JSON.parse(body).id), 3) + ".gif"
-
-                    }
-                ]
-            });
-        }
-        else {
-            bot.reply(message, "Pokemon not found! Did you type its name correctly?");
-        }
-    });*/
-
-    // New sie that works! :)
-
+    // Check to see if link exists if not give error
     var url = "http://www.pokestadium.com/sprites/xy/"+pokemon+".gif";
     request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -91,7 +76,32 @@ controller.hears([/hello/i], ['direct_message', 'direct_mention', 'mention', 'am
         "Type _I choose you_ or _go_ followed by a pokemon's name to summon a pokemon!\n" +
         "Or if you wish to fight me than type _battle_ in a direct message to me!");
 });
+controller.hears('interactive', 'direct_message', function(bot, message) {
 
+    bot.reply(message, {
+        attachments:[
+            {
+                title: 'Do you want to interact with my buttons?',
+                callback_id: '123',
+                attachment_type: 'default',
+                actions: [
+                    {
+                        "name":"yes",
+                        "text": "Yes",
+                        "value": "yes",
+                        "type": "button",
+                    },
+                    {
+                        "name":"no",
+                        "text": "No",
+                        "value": "no",
+                        "type": "button",
+                    }
+                ]
+            }
+        ]
+    });
+});
 controller.hears([/battle/i], ['direct_message'], function (bot, message) {
     require('./hears/battle.js').startBattle(bot, message);
 });
